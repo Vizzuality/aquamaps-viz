a44be70ee14d6d57d06794a0143032b5c06bfc9d
pjosh <jorgegar@gmail.com>
2017-03-03 13:20:56 +0100

    * Picture slider * Show picture modal

---
+++ species_data.php
@@ -0,0 +1,88 @@
+<?php
+$species = [
+  [
+    "key" => "callinectes_sapidus",
+    "name" => "Callinectes sapidus",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/callinectes_sapidus/exp_7_pic_W-Bch-107379_native.jpg"
+      ],
+      [
+        "name" => "Suitable Habitat",
+        "url" => "/images/species/callinectes_sapidus/exp_7_pic_W-Bch-107379_suitable.jpg"
+      ]
+    ]
+  ],
+  [
+    "key" => "crassostrea_gigas",
+    "name" => "Crassostrea gigas",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/crassostrea_gigas/exp_33_pic_W-Msc-140656_suitable.jpg"
+      ],
+      [
+        "name" => "Suitable Habitat",
+        "url" => "/images/species/crassostrea_gigas/exp_33_pic_W-Msc-140656.jpg"
+      ]
+    ]
+  ],
+  [
+    "key" => "mnemiopsis_leidyi",
+    "name" => "Mnemiopsis leidyi",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/mnemiopsis_leidyi/exp_16_pic_URM-M-196_native.jpg"
+      ],
+      [
+        "name" => "Suitable Habitat",
+        "url" => "/images/species/mnemiopsis_leidyi/exp_16_pic_URM-M-196_suitable.jpg"
+      ]
+    ]
+  ],
+  [
+    "key" => "apogon_imberbis",
+    "name" => "Apogon Imberbis",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/apogon_imberbis/pic_Fis-23372_native.jpg"
+      ],
+      [
+        "name" => "2100 Native Range",
+        "url" => "/images/species/apogon_imberbis/pic_Fis-23372_y2100.jpg"
+      ]
+    ]
+  ],
+  [
+    "key" => "arnoglossus_ruepelli",
+    "name" => "Arnoglossus Ruepelli",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/arnoglossus_ruepelli/exp_24_pic_Fis-136960_native.jpg"
+      ],
+      [
+        "name" => "2100 Native Range",
+        "url" => "/images/species/arnoglossus_ruepelli/exp_24_pic_Fis-136960_y2100.jpg"
+      ]
+    ]
+  ],
+  [
+    "key" => "engraulis_encrasicolus",
+    "name" => "Engraulis Encrasicolus",
+    "pictures" => [
+      [
+        "name" => "Native Range",
+        "url" => "/images/species/engraulis_encrasicolus/pic_Fis-22756_native.jpg"
+      ],
+      [
+        "name" => "2100 Native Range",
+        "url" => "/images/species/engraulis_encrasicolus/pic_Fis-22756_y2100.jpg"
+      ]
+    ]
+  ]
+];
+?>
